3d0a2219d1aadd619febc22e9c903a0d4d3ba18b
Jeanne Salle <jeanne.salle@yahoo.fr>
2026-06-28 14:00:59 +0200

    Added 'The Swallows of Kabul' to the reading list

--- content/reading/index.md
+++ content/reading/index.md
@@ -11,6 +11,17 @@ Reading is one of my favorite things, and I have more lists of books than I'll p
 
 <div class="book-card">
 <div class="book-cover">
+<img src="https://upload.wikimedia.org/wikipedia/en/7/7a/The_Swallows_of_Kabul.jpg" alt="The Swallows of Kabul">
+</div>
+<div class="book-detail">
+<h3>The Swallows of Kabul</h3>
+<p class="book-author">Yasmina Khadra</p>
+<p class="book-summary"></p>
+</div>
+</div>
+
+<div class="book-card">
+<div class="book-cover">
 <img src="https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1597056415i/54870256.jpg" alt="There is no Antimemetics Division">
 </div>
 <div class="book-detail">
--- quartz/components/SidebarNav.tsx
+++ quartz/components/SidebarNav.tsx
@@ -8,7 +8,9 @@ const SidebarNav: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
       <nav class="sidebar-nav">
         <a href={baseDir}>About me</a>
         <a href={`${baseDir}/writing`}>Writing</a>
-        <a href={`${baseDir}/reading`}>Reading List</a>
+        <a href={`${baseDir}/reading`} class="sidebar-nav-icon" title="Reading List">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z"/></svg>
+        </a>
       </nav>
       <div class="sidebar-links">
         <a href="https://scholar.google.com/citations?user=qr-gsXMAAAAJ&hl=en" target="_blank" rel="noopener" title="Google Scholar">
@@ -44,6 +46,15 @@ SidebarNav.css = `
 }
 
 .sidebar-nav a:hover {
+  color: var(--secondary);
+}
+
+.sidebar-nav a.sidebar-nav-icon {
+  color: var(--dark);
+  padding: 0.1rem 0;
+}
+
+.sidebar-nav a.sidebar-nav-icon:hover {
   color: var(--secondary);
 }
 
